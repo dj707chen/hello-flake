772d1217e551dbe657d2975f656afecc9879ea38
DJ Chen <80285637+dj707chen@users.noreply.github.com>
2025-12-07 15:39:52 -0600

    Save initial version

---
+++ commands.sh
@@ -0,0 +1,6 @@
+
+# https://www.tweag.io/blog/2020-05-25-flakes/#writing-your-first-flake
+
+echo 'int main() { printf("Hello World"); }' > hello.c
+
+# To turn this Git repository into a flake, we add a file named flake.nix at the root of the repository with the following contents:
